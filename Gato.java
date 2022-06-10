753af10d1e82f42bb52aeb56e56846d0e111f1a5
Juninho <junio_kabal@hotmail.com>
2022-06-10 00:18:58 -0300

    adicionei classe Gato

---
+++ Gato.java
@@ -0,0 +1,15 @@
+package estudoprogramacao;
+
+public class Gato {
+    // atributos
+    public String id;
+    publi String nome;
+    public String cor_pelo;
+    public int idade;
+    public String raca;
+    public int quantidade_Patas;
+    public boolean temRabo;
+    public double peso;
+
+    // metodos
+}
---
+++ Gato2.java
@@ -0,0 +1,13 @@
+public class Gato2 {
+    // atributos
+    public String id;
+    public String nome;
+    public String cor_pelo;
+    public int idade;
+    public String raca;
+    public int quantidade_Patas;
+    public boolean temRabo;
+    public double peso;
+
+    // metodos
+}
